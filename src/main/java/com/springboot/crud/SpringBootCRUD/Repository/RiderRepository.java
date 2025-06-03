48e0e01f5e69e7c975e3fd04ed94e245e2e535d3
Rishabh Gupta <65425244+Rishabh13052@users.noreply.github.com>
2025-06-03 19:36:48 +0530

    merge code in intermediate master branch (#5)
    * refactor UserController.java file.
    
    * Configure Spring Batch to copy data from database to text file, (#2)
    
    * Added existing Code in Repo

--- src/main/java/com/springboot/crud/SpringBootCRUD/Repository/RiderRepository.java
+++ src/main/java/com/springboot/crud/SpringBootCRUD/Repository/RiderRepository.java
@@ -1,8 +1,25 @@
 package com.springboot.crud.SpringBootCRUD.Repository;
 
+import com.springboot.crud.SpringBootCRUD.DTO.OutRiderDto;
+import com.springboot.crud.SpringBootCRUD.DTO.RiderDto;
 import com.springboot.crud.SpringBootCRUD.Entity.Rider;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface RiderRepository extends JpaRepository<Rider, Long> {
+
+    @Query( value = "SELECT new com.springboot.crud.SpringBootCRUD.DTO.OutRiderDto (riderCode, riderDesc, riderPT) from Rider r where r.riderPT = ?1")
+    List<OutRiderDto> getListOfRider(Long riderPt);
+
+    @Modifying
+    @Transactional
+    @Query( value = "UPDATE Rider r set r.riderpt = :riderPt where r.rider_code = :riderCode", nativeQuery = true)
+    int getUpdatedListOfRider(@Param("riderCode") String riderCode, @Param("riderPt") Long riderPt);
 
 }
